9669d7c8f89bb152839dcb9b247c3915306c1dab
Harmehel Singh <harmehel@hs-mbp.local>
2026-03-25 14:09:28 -0700

    added func for vector magnitude

--- Vector.java
+++ Vector.java
@@ -10,8 +10,12 @@ public class Vector {
 	public static double[] subtract(double[] A, double[] B){
 		return new double[]{A[0] - B[0], A[1] - B[1]};
 	}
-	
+
 	public static double[] add(double[] A, double[] B){
 		return new double[]{A[0] + B[0], A[1] + B[1]};
+	}
+
+	public static double mag(double[] A){
+		return Math.sqrt(A[0] * A[0] + A[1] * A[1]);
 	}
 }
